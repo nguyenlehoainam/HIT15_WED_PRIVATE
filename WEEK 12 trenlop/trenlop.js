fa8d86b4aba27eeb8c0507501b0649ea40c61a34
nguyenlehoainam <namngyenhoai21@gmail.com>
2025-02-23 06:22:17 +0700

    BTVN

---
+++ WEEK 12 trenlop/trenlop.js
@@ -0,0 +1,4 @@
+const text = "JavaScript is fun";
+console.log(text.slice(0, 10)); // "JavaScript"
+console.log(text.slice(11)); // "is fun"
+console.log(text.slice(-3)); // "fun" (chỉ số âm tính từ cuối chuỗi)
